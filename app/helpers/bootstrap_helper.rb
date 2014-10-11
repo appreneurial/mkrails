06c79a8149b67eec8e5c90a51b139e7ed8556180
Jason Green <jason.green@appreneurial.co>
2014-10-11 19:43:33 +1100

    Implement Bootstrap Nav Support (closes #9)

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -30,6 +30,27 @@ module BootstrapHelper
 		alerts.join("\n").html_safe
 	end
 
+	def bootstrap_nav(options = {}, &block)
+		ul_classes = ["nav"]
+		ul_classes << "nav-tabs" if options[:tabs] == true
+		ul_classes << "nav-pills" if options[:pills] == true
+		ul_classes << "nav-stacked" if options[:stacked] == true
+		ul_classes << "nav-justified" if options[:justified] == true
+		ul_classes << "navbar-nav" if options[:navbar] == true
+		ul_classes << (options[:navbar] == true ? "navbar-#{options[:pull].to_s}" : "pull-#{options[:pull].to_s}") if options[:pull]
+
+		content_tag("ul", nil, class: ul_classes.join(" ")) do
+			capture(&block) if block_given?
+		end
+	end
+
+	def bootstrap_nav_link(text, destination, options = {})
+		li_classes = []
+		li_classes << "active" if options.delete(:active) == true
+
+		content_tag("li", link_to(text, destination, options), class: li_classes.join(" "))
+	end
+
 	def bootstrap_navbar(options = {}, &block)
 		nav_classes = ["navbar"]
 		nav_classes << (options[:inverse] == true ? "navbar-inverse" : "navbar-default")
@@ -52,22 +73,6 @@ module BootstrapHelper
 				end
 			end
 		end
-	end
-
-	def bootstrap_navbar_nav(options = {}, &block)
-		ul_classes = ["nav", "navbar-nav"]
-		ul_classes << "navbar-#{options[:pull].to_s}" if options[:pull]
-
-		content_tag("ul", nil, class: ul_classes.join(" ")) do
-			capture(&block) if block_given?
-		end
-	end
-
-	def bootstrap_nav_link(text, destination, options = {})
-		li_classes = []
-		li_classes << "active" if options.delete(:active) == true
-
-		content_tag("li", link_to(text, destination, options), class: li_classes.join(" "))
 	end
 
 	def bootstrap_page_header(&block)
@@ -113,6 +118,15 @@ module BootstrapHelper
 			content_tag("table", nil, class: table_classes.join(" ")) do
 				capture(&block) if block_given?
 			end
+		end
+	end
+
+	# Deprecated Functions
+
+	def bootstrap_navbar_nav(options = {}, &block)
+		options << {navbar: true}
+		bootstrap_nav(options) do
+			capture(&block) if block_given?
 		end
 	end
 end
